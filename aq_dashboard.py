aabb0e086c42bb25a0ffcc42ef38da19a710d267
medamer <edamer.mo@gmail.com>
2020-06-19 09:29:32 -0700

    whatever

---
+++ aq_dashboard.py
@@ -0,0 +1,36 @@
+"""OpenAQ Air Quality Dashboard with Flask."""
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+APP = Flask(__name__)
+
+
+@APP.route('/')
+def root():
+    utc_datetime=str([('2019-03-08T00:00:00.000Z', 8.13), 
+    ('2019-03-07T23:00:00.000Z', 8.13)])
+    return utc_datetime
+
+
+APP.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+DB = SQLAlchemy(APP)
+
+
+class Record(DB.Model):
+    id = DB.Column(DB.Integer, primary_key=True)
+    datetime = DB.Column(DB.String(25))
+    value = DB.Column(DB.Float, nullable=False)
+
+    def __repr__(self):
+        return 'TODO - write a nice representation of Records'
+
+
+@APP.route('/refresh')
+def refresh():
+    """Pull fresh data from Open AQ and replace existing data."""
+    DB.drop_all()
+    DB.create_all()
+    # TODO Get data from OpenAQ, make Record objects with it, and add to db
+    DB.session.commit()
+    return 'Data refreshed!'
