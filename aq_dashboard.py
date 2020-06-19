7f08a02fdd20a60c2679dcf61ebf2b2c581ce097
medamer <edamer.mo@gmail.com>
2020-06-19 15:05:49 -0700

    fixed the bugs

--- aq_dashboard.py
+++ aq_dashboard.py
@@ -1,33 +1,18 @@
 """OpenAQ Air Quality Dashboard with Flask."""
-from flask import Flask, request
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 import openaq
 
 APP = Flask(__name__)
+APP.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+DB = SQLAlchemy(APP)
+api=openaq.OpenAQ()
+#SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 @APP.route('/')
 def root():
-    utc_datetime = request.form['datetime']
-    value= request.form['value']
-    return utc_datetime
-
-
-APP.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-DB = SQLAlchemy(APP)
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
-
-class Record(DB.Model):
-    id = DB.Column(DB.Integer, primary_key=True)
-    datetime = DB.Column(DB.String(25))
-    value = DB.Column(DB.Float, nullable=False)
-
-    def __repr__(self):
-        return 'DATABASE CREATED'
-
+    return str(Record.query.filter(Record.value >= 10).all())
 
 @APP.route('/refresh')
 def refresh():
@@ -35,8 +20,23 @@ def refresh():
     DB.drop_all()
     DB.create_all()
     # TODO Get data from OpenAQ, make Record objects with it, and add to db
-    api=openaq.OpenAQ()
-    status, resp = api.measurements()
-    record=DB.resp['results']
+    for result in get_results():
+        record = Record(datetime=result[0], value=result[1])
+        DB.session.add(record)
     DB.session.commit()
     return 'Data refreshed!'
+
+def get_results():
+    _, body = api.measurements(city='Los Angeles', parameter='pm25')
+    results=[]
+    for result in body['results']:
+        results.append((result['date']['utc'], result['value']))
+    return results
+
+class Record(DB.Model):
+    id = DB.Column(DB.Integer, primary_key=True)
+    datetime = DB.Column(DB.String(25))
+    value = DB.Column(DB.Float, nullable=False)
+
+    def __repr__(self):
+        return '< Time {} --- Value {} >'.format(self.datetime, self.value)
